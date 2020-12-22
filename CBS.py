5bf31b0a937cb38bae4630efd0daa7a540b2e272
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2020-12-22 11:54:29 +0000

    Several changes regarding removing CBS directory. See description
    Fixed a file path issue in moveDirectoryContentsUpOne that prevented the next file up from being selected.
    
    Split removeCbsTempDirectory into 2 functions:
    requestToRemoveCbsDirectory
    removeTempDirectory
    
    The software can now ask the user if they want to keep the backed up files in a file named "CascadingBackupSystem" or instead store the files directly in the destination

--- CBS.py
+++ CBS.py
@@ -12,23 +12,27 @@ def generateDirectoryPathFromDialogue():
 
 # moves all directories and files in src up a directory
 def moveDirectoryContentsUpOne(src):
-    dest = src + ".."
+    dest = src + "/.."
     fileNames = os.listdir(src)
     for fileName in fileNames:
         fileSrc = os.path.join(src, fileName)
         shutil.move(fileSrc, dest)
 
-# removes "CascadingBackupSystem" directory from dest if the user requests
-def removeCbsTempDirectory(path):
+def requestToRemoveCbsDirectory(path):
+    path = path + "/CascadingBackupSystem"
     continueLoop = 1
     while continueLoop == 1:
-        removeDirectory = input("Would you like the copied files to reside in a CascadingBackupSystem directory? Please enter yes or no: ")
-        removeDirectory = removeDirectory.lower()
-        if removeDirectory == "no" or removeDirectory == "yes":
-            if removeDirectory == "no":
-                moveDirectoryContentsUpOne(path)
-                shutil.rmtree(path)
+        keepDirectory = input("Would you like the copied files to reside in a CascadingBackupSystem directory? Please enter yes or no: ")
+        keepDirectory = keepDirectory.lower()
+        if keepDirectory == "no" or keepDirectory == "yes":
+            if keepDirectory == "no":
+                removeTempDirectory(path)
             continueLoop = 0
+
+# removes "CascadingBackupSystem" directory from dest if the user requests
+def removeTempDirectory(path):
+    moveDirectoryContentsUpOne(path)
+    shutil.rmtree(path)
 
 def copyTree(src,dest):
     dest = dest + "/CascadingBackupSystem"
@@ -38,3 +42,4 @@ if __name__ == "__main__":
     src = generateDirectoryPathFromDialogue()
     dest = generateDirectoryPathFromDialogue()
     copyTree(src,dest)
+    requestToRemoveCbsDirectory(dest)
